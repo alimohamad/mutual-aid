f585c753fa8c191b79264e3a34d2081bcef333ba
Ali Mohamad <alimohamad1122@gmail.com>
2021-07-05 19:12:01 -0400

    Added Firebase get by url params.

--- src/components/FormType.js
+++ src/components/FormType.js
@@ -1,31 +1,22 @@
 import React, { useState, useEffect } from 'react'
 
 import EmptySubjectLine from './EmptySubjectLine'
-import CauseForm from "./CauseForm"
+import CauseForm from './CauseForm'
+import Firebase from './Firebase'
 
 const FormType = ({ match }) => {
     const [org, setOrg] = useState({})
     useEffect(() => {
         // match.params.org => get org name, pass to Firebase query, set org.
-
-        setOrg({
-            name: match.params.org,
-            format: "ESL",
-            account: "thematchfund@gmail.com",
-            body: " The fight against police brutality and the militarization of law enforcement in the United States is happening as we speak. Some of us can't protest, but we can donate—through The Match Fund, your contribution to any organization of your choosing can be doubled through donation matching programs at tech companies by employees who are passionate about seeing the change we're fighting for. You send us a donation and your donation is gifted to one of our volunteers, who match your gift through their employee donation matching program to work around donation matching programs. After your donation, you'll be sent a confirmation of your matched donation. Note that we match donations in batches, so confirmation will not be immediate.",
-            causes: [
-                {
-                    title: "P",
-                    amount: 300.00,
-                    blurb: "help P with rent"
-                },
-                {
-                    title: "A",
-                    amount: 100.00,
-                    blurb: "help A with groceries"
-                },
-            ]
-        })
+        Firebase.firestore()
+        .collection('funds')
+        .where("referenceName", "==", match.params.org)
+        .get()
+        .then((snapshot) => {
+            if(snapshot.docs.length === 1) {
+                setOrg(snapshot.docs[0].data())
+            }
+        });  
     }, [])
     
 
